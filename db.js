2ba09e6c96b21467677cfa3fa15e61e90cddec3e
bill-burgess <wmhburgess@gmail.com>
2017-09-28 11:43:05 +1300

    displat user profile

--- db.js
+++ db.js
@@ -6,8 +6,10 @@ module.exports = {
 
 function getUsers (connection) {
   return connection('users').select()
+
 }
 
 function getUser (id, connection) {
-  return connection('users').where('id', id)
+  return connection('users').where('id', id) // this returns an array
+  .first()
 }
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router()
 var db = require('../db')
 
 router.get('/', function (req, res) {
+  
   db.getUsers(req.app.get('connection'))
     .then(function (users) {
       res.render('index', { users: users })
@@ -14,13 +15,25 @@ router.get('/', function (req, res) {
 })
 
 router.get('/users/:id', (req, res) => {
-  db.getUsers(req.app.get('connection'))
+
+  db.getUser(req.params.id, req.app.get('connection'))
+    //the user below is a parameter that gets passed through .then, it can be any given name
     .then(user => {
-      res.render('users', user)
+      res.render('user_profile', user)
     })
     .catch(err => {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 })
+
+router.get('/users/new' (req, res) => {
+
+  db.getUsers(req.app.get('connection'))
+    .then()
+
+  res.render('add_user', )
+})
+
+
 
 module.exports = router
